6be36375385ae041a06c11aa9bc17a28e47b253f
root <root@SecurityCraze.secure.com>
2020-12-24 21:27:12 +0530

    mainBranch

--- newcode.py
+++ newcode.py
@@ -1,5 +1,7 @@
 #! /bin/python
 
-name=input("what is your name: ")
-print(name)
-print ("this is my first code")
+name=input("what is your Father name: ")
+print("your Father name: " +name)
+
+age=input("what is your Father age: ")
+print("your Father age: " +age)
